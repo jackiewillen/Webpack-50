e057c9e8c7816d2b62c734dfd674933df303868e
jackieyin <jackieyin@tencent.com>
2019-07-13 12:33:26 +0800

    准备好babel-loader需要配置前的代码

---
+++ Loader/BableLoader/src/index.js
@@ -0,0 +1,8 @@
+let a = [1,2,3];
+let b = [...a, 4];
+
+let c = 'my';
+let d = 'name';
+let f = 'is';
+
+let sentence = `${c} first ${d} ${f} yin`;
---
+++ Loader/BableLoader/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',    //webpack需要一个唯一的入口js文件，src目录是自行创建的专用于存放原始代码的目录
+  output: {
+    filename: 'bundle.js',    //bundle.js是打包后输出的js文件
+    path: path.resolve(__dirname, 'dist') //这里用来设置输出的位置。
+  }
+};
